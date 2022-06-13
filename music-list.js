756e80219a59abcbdbea920e7610248989ac131e
Radka Krenova <radka.krenov@gmail.com>
2022-06-13 09:31:05 +0200

    my blog page

---
+++ music-list.js
@@ -0,0 +1,57 @@
+let allMusic = [
+  {
+    name: "Forever Young",
+    artist: "Boy In Space",
+    img: "#",
+    src: "song-1"
+  },
+  {
+    name: "I Will",
+    artist: "Jeremy Newcastle",
+    img: "#",
+    src: "song-2"
+  },
+  {
+    name: "Far Away",
+    artist: "Whale City",
+    img: "#",
+    src: "song-3"
+  },
+  {
+    name: "Rocks at Your Window",
+    artist: "Yelawolf",
+    img: "#",
+    src: "song-4"
+  },
+  {
+    name: "Born To Be Wild",
+    artist: "Natalie Taylor",
+    img: "#",
+    src: "song-5"
+  },
+  {
+    name: "One More Time",
+    artist: "Ray Lorraine",
+    img: "#",
+    src: "song-6"
+  },
+  {
+    name: "Thank You",
+    artist: "Celine Dion",
+    img: "#",
+    src: "song-7"
+  },
+  {
+    name: "Fires",
+    artist: "Jordan St. Cyr",
+    img: "#",
+    src: "song-8"
+  },
+  {
+    name: "Hotel Walls",
+    artist: "Smith & Thell",
+    img: "#",
+    src: "song-9"
+  },
+
+];
